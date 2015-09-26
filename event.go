01d1313136077ba0000d7320167f5b3a323399b5
Tyler Bunnell <tylerbunnell@gmail.com>
2015-09-25 19:51:19 -0600

    Custom event time type for decoding time in event

--- event.go
+++ event.go
@@ -32,8 +32,22 @@ var EventType = struct {
 type Object struct {
 	ID          string    `json:"id"`
 	Status      string    `json:"status"`
-	CompletedAt time.Time `json:"completed_at"`
+	CompletedAt EventTime `json:"completed_at"`
 	Href        string    `json:"href"`
+}
+
+var eventTimeFormat = "2006-01-02 15:04:05 MST"
+
+type EventTime time.Time
+
+func (et *EventTime) UnmarshalJSON(data []byte) error {
+	var err error
+	t, err := time.Parse(eventTimeFormat, string(data))
+	*et = EventTime(t)
+	if err != nil {
+		*et = EventTime(time.Time{})
+	}
+	return nil
 }
 
 type Event struct {
